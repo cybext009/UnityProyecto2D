4ea0e5a1a6f68061e55c1cd3b453294c06c89b42
cybext009 <202104862@udv.edu.gt>
2026-02-24 20:23:06 -0600

    Add project files.

---
+++ Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Saludar();
+    }
+
+    public void Saludar()
+    {
+        Debug.Log("Hola Mundo");
+    }
+}
