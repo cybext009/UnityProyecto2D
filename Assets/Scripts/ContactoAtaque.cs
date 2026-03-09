e0e81d58f274ae4db82944c1dea726b40f987230
cybext009 <202104862@udv.edu.gt>
2026-03-09 16:31:48 -0600

    Logica de daño y salud

---
+++ Assets/Scripts/ContactoAtaque.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Equipo))]
+public class ContactoAtaque : MonoBehaviour
+{
+    [SerializeField] private float cantAtaque = 1f;
+    private Equipo equipo;
+
+    void Awake()
+    {
+        equipo = GetComponent<Equipo>();
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+
+    {
+
+        Debug.Log("Colision detectada");
+
+        if (!other.gameObject.TryGetComponent<Salud>(out Salud saludDelOtro))
+        {
+            return;
+        }
+
+        if (!other.gameObject.TryGetComponent<Equipo>(out Equipo equipoDelOtro))
+        {
+            return;
+        }
+
+        if (equipoDelOtro.EquipoActual == equipo.EquipoActual)
+        {
+            return;
+        }
+
+        saludDelOtro.PerderSalud(cantAtaque);
+
+        // Muestra la vida restante
+        Debug.Log("Vida restante: " + saludDelOtro.ObtenerSalud());
+    }
+}
---
+++ Assets/Scripts/Equipo.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Equipo : MonoBehaviour
+{
+    [SerializeField] EquipoEnum equipo = EquipoEnum.Trampas;
+    public EquipoEnum EquipoActual
+    {
+        get {  return equipo; }
+        private set { equipo = value; }
+    }
+}
